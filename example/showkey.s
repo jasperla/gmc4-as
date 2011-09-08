bbf1fe147a94ebf4b8974316885a63558c2c3cb5
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2011-09-08 23:46:25 +0200

    Add more body to the README

--- example/showkey.s
+++
@@ -1,9 +0,0 @@
-; Some rambling
-KA	; More comments
-TIY E
-AM
-TIY F
-TIA 0
-AM
-CAL SUND
-JUMP 00
---
+++ examples/showkey.s
@@ -0,0 +1,9 @@
+; Show the key entered on the display
+KA	; Read the key
+TIY E
+AM
+TIY F
+TIA 0
+AM
+CAL SUND ; Display the key
+JUMP 00
